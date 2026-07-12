93840630b648c86cdedc61015fd03a16eea1e38e
giorg19851-lgtm <giorg19851@gmail.com>
2026-07-11 20:11:44 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,5 @@
-const CACHE = "vgik-test-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./db.js",
-  "./manifest.json",
-  "./assets/icon-192.png",
-  "./assets/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
-});
+const CACHE="vgik-test-v3";
+const ASSETS=["./","./index.html","./style.css","./app.js","./db.js","./manifest.json","./assets/icon.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))});
