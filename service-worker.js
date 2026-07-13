3e970136d547075a3880bc930c3dc11a2505b0eb
giorg19851-lgtm <giorg19851@gmail.com>
2026-07-13 06:29:54 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="vgik-test-v13";
-const ASSETS=["./index.html?v=13","./style.css?v=13","./app.js?v=13","./db.js?v=13","./manifest.json?v=13","./assets/icon.svg"];
+const CACHE="vgik-test-v14";
+const ASSETS=["./index.html?v=14","./style.css?v=14","./app.js?v=14","./db.js?v=14","./manifest.json?v=14","./assets/icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))});
